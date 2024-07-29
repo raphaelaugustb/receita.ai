401c3443581a710e6a9e3fc53446c7d555f663c9
raphaelaugustb <travassosraphael12@gmail.com>
2024-07-29 20:27:13 -0300

    Create exception for invalid fields

---
+++ backend/receitas.ai/src/main/java/com/leah/receitas/ai/exception/MissingFieldsException.java
@@ -0,0 +1,7 @@
+package com.leah.receitas.ai.exception;
+
+public class MissingFieldsException  extends  RuntimeException {
+    public MissingFieldsException(String message) {
+        super(message);
+    }
+}
