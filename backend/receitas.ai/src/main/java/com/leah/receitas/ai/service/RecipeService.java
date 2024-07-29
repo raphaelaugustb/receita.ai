687c91045fc249119aba50b123efe53d588f5a71
raphaelaugustb <travassosraphael12@gmail.com>
2024-07-29 19:38:36 -0300

    TODO

---
+++ backend/receitas.ai/src/main/java/com/leah/receitas/ai/service/RecipeService.java
@@ -0,0 +1,13 @@
+package com.leah.receitas.ai.service;
+
+public class RecipeService {
+    //TODO
+    // Implementar função de criação de receitas publicas ou privadas
+    // Implementar métodos crud
+    // Alimentar API com receitas externas
+    // Permitir usuário buscar receitas pelo nome
+    // Listar apenas receitas não privadas
+    // Permitir o usuário Salvar receitas de terceiros
+    // Implementar IA
+    // Verificar dados com DTO. mapear saídas e entradas
+}
---
+++ backend/receitas.ai/src/main/java/com/leah/receitas/ai/service/UserService.java
@@ -0,0 +1,9 @@
+package com.leah.receitas.ai.service;
+
+public class UserService {
+    //TODO
+    // Implementar métodos CRUD do usuário
+    // Verificar Request com DTO
+    // Listas as listas do usuário pelo nome e permitir que apenas liste a lista publica
+    // Listar o perfil do usuário pelo nome
+}
