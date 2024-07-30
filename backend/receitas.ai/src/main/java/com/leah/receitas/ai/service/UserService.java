96d2d450f66661da9f6f52869489b219c56a4852
raphaelaugustb <travassosraphael12@gmail.com>
2024-07-30 15:05:42 -0300

    Code Review on user service

--- backend/receitas.ai/src/main/java/com/leah/receitas/ai/service/UserService.java
+++ backend/receitas.ai/src/main/java/com/leah/receitas/ai/service/UserService.java
@@ -10,11 +10,13 @@ import com.leah.receitas.ai.exception.MissingFieldsException;
 import com.leah.receitas.ai.exception.UserNotFoundException;
 import com.leah.receitas.ai.repository.UserRepository;
 import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Service
 public class UserService {
     //TODO
     // Implementar métodos CRUD do usuário
@@ -28,51 +30,56 @@ public class UserService {
     }
 
     public User verifyUser(UUID userId) {
-        User user =  userRepository.findById(userId).orElse(null);
+        User user = userRepository.findById(userId).orElse(null);
         if (user == null)
             throw new UserNotFoundException("User not Found");
         return user;
     }
+
     public UserResponse createUser(UserRequest userRequest) {
-       User user = new User();
-       user.setUsername(userRequest.username());
-       user.setPassword(userRequest.password());
-       user.setEmail(userRequest.email());
-       user.setRecipeList(new ArrayList<>());
-       user.setSavedRecipeList(new ArrayList<>());
-       user.setTempRecipeList(new ArrayList<>());
-
-       if (user.getEmail() != null && user.getPassword()!= null && user.getUsername() != null){
-            userRepository.save(user);
-       } else {
-           throw new MissingFieldsException("Some of the fields are missing");
-       }
-
-       return new UserResponse(user.getUsername(), user.getEmail(), user.getPassword(), user.getRecipeList(), user.getSavedRecipeList());
-    }
-    public void deleteUser(UUID userId) {
-      User user = verifyUser(userId);
-      userRepository.delete(user);
-    }
-    public UserResponse getUserInfo(UUID userId){
-        User user = verifyUser(userId);
-        return new UserResponse(user.getUsername(), user.getEmail(), user.getPassword(), user.getRecipeList(), user.getSavedRecipeList());
-    }
-     public UserPublicInfo getUserPublicInfo(String username){
-        User user = userRepository.findByUsername(username);
-        if (user == null)
-            throw new UserNotFoundException("User not Found");
-         List<Recipe> recipeList = user.getRecipeList().stream().filter(recipe -> !recipe.getIsPrivate()).toList();
-        return new UserPublicInfo(username, recipeList);
-
-     }
-     public UserResponse updateUser(UUID userId, UserRequest userRequest) {
-        User user = verifyUser(userId);
-         if (userRequest == null)
-             throw new InvalidRequestException("Request cannot be null");
+        User user = new User();
         user.setUsername(userRequest.username());
         user.setPassword(userRequest.password());
         user.setEmail(userRequest.email());
-         return new UserResponse(user.getUsername(), user.getEmail(), user.getPassword(), user.getRecipeList(), user.getSavedRecipeList());
-     }
+        user.setRecipeList(new ArrayList<>());
+        user.setSavedRecipeList(new ArrayList<>());
+        user.setTempRecipeList(new ArrayList<>());
+
+        if (user.getEmail() != null && user.getPassword() != null && user.getUsername() != null) {
+            userRepository.save(user);
+        } else {
+            throw new MissingFieldsException("Some of the fields are missing");
+        }
+
+        return new UserResponse(user.getUsername(), user.getEmail(), user.getPassword(), user.getRecipeList(), user.getSavedRecipeList());
+    }
+
+    public void deleteUser(UUID userId) {
+        User user = verifyUser(userId);
+        userRepository.delete(user);
+    }
+
+    public UserResponse getUserInfo(UUID userId) {
+        User user = verifyUser(userId);
+        return new UserResponse(user.getUsername(), user.getEmail(), user.getPassword(), user.getRecipeList(), user.getSavedRecipeList());
+    }
+
+    public UserPublicInfo getUserPublicInfo(String username) {
+        User user = userRepository.findByUsername(username);
+        if (user == null)
+            throw new UserNotFoundException("User not Found");
+        List<Recipe> recipeList = user.getRecipeList().stream().filter(recipe -> !recipe.getIsPrivate()).toList();
+        return new UserPublicInfo(username, recipeList);
+
+    }
+
+    public UserResponse updateUser(UUID userId, UserRequest userRequest) {
+        User user = verifyUser(userId);
+        if (userRequest == null)
+            throw new InvalidRequestException("Request cannot be null");
+        user.setUsername(userRequest.username());
+        user.setPassword(userRequest.password());
+        user.setEmail(userRequest.email());
+        return new UserResponse(user.getUsername(), user.getEmail(), user.getPassword(), user.getRecipeList(), user.getSavedRecipeList());
+    }
 }
