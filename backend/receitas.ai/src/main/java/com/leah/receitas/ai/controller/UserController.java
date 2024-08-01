fcadcd0381a871e9cd247e38387a0275efa68227
raphaelaugustb <travassosraphael12@gmail.com>
2024-08-01 01:24:06 -0300

    Method for update user info

--- backend/receitas.ai/src/main/java/com/leah/receitas/ai/controller/UserController.java
+++ backend/receitas.ai/src/main/java/com/leah/receitas/ai/controller/UserController.java
@@ -19,27 +19,36 @@ public class UserController {
         this.userService = userService;
     }
 
+    //Criar um novo usuário
     @PostMapping("/register")
     public ResponseEntity<UserResponse> register(@RequestBody UserRequest userRequest) {
         UserResponse userResponse = userService.createUser(userRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(userResponse);
     }
 
+    // Deletar a conta de um usuário
     @DeleteMapping("{userId}/account")
     public ResponseEntity<String> deleteAccount(@PathVariable UUID userId) {
         userService.deleteUser(userId);
         return ResponseEntity.status(HttpStatus.ACCEPTED).body("Account successfully deleted");
     }
 
+    //Receber os dados públicos do usuário (Receitas que são públicas e o seu nome)
     @GetMapping("{username}/profile")
     public ResponseEntity<UserPublicInfo> userPublicProfile(@PathVariable String username) {
         UserPublicInfo userPublicInfo = userService.getUserPublicInfo(username);
         return ResponseEntity.status(HttpStatus.FOUND).body(userPublicInfo);
     }
 
+    //Receber todos os dados do usuário
     @GetMapping( "{userId}/account")
     public ResponseEntity<UserResponse> userAccount (@PathVariable UUID userId) {
         UserResponse userResponse = userService.getUserInfo(userId);
         return ResponseEntity.status(HttpStatus.FOUND).body(userResponse);
+    }
+
+    @PutMapping("{userId}/account")
+    public ResponseEntity<UserResponse> updateUser(@PathVariable UUID userId, @RequestBody UserRequest userRequest) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.updateUser(userId, userRequest));
     }
 }
