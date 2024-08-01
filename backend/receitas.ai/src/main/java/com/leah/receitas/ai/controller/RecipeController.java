6f75116d8350fa247a5736eedff55ee8765d58fe
raphaelaugustb <travassosraphael12@gmail.com>
2024-08-01 01:01:45 -0300

    create method for save recipes and list saved recipes

--- backend/receitas.ai/src/main/java/com/leah/receitas/ai/controller/RecipeController.java
+++ backend/receitas.ai/src/main/java/com/leah/receitas/ai/controller/RecipeController.java
@@ -7,25 +7,49 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
 public class RecipeController {
 
     private final RecipeService recipeService;
-
+    //TODO
+    // Melhorar qualidades da rotas
+    // Separar serviços para listar salvos e lista do usuário
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
-
+    @GetMapping({"/recipe"})
+    public ResponseEntity<List<RecipeResponse>> getRecipePublic(@RequestParam(name = "name") String recipeName) {
+        return ResponseEntity.status(HttpStatus.FOUND).body(recipeService.getRecipesByNamePublic(recipeName));
+    }
+    @GetMapping("{userId}/recipe")
+    public ResponseEntity<List<RecipeResponse>> getUserRecipe(@PathVariable UUID userId, @RequestParam(name = "name") String recipeName) {
+        return ResponseEntity.status(HttpStatus.FOUND).body(recipeService.getUserRecipesByName(userId, recipeName));
+    }
     @PostMapping("{userId}/recipe")
     public ResponseEntity<RecipeResponse> createRecipe(@PathVariable UUID userId, @RequestBody RecipeRequest recipeRequest) {
         return ResponseEntity.status(HttpStatus.CREATED).body(recipeService.createRecipe(recipeRequest, userId));
+    }
+    @DeleteMapping("{userId}/recipe/save/{recipeId}")
+    public ResponseEntity<String> removeSavedRecipe(@PathVariable UUID userId, @PathVariable long recipeId) {
+        recipeService.removeRecipeSaved(userId,recipeId);
+        return ResponseEntity.status(HttpStatus.OK).body("Recipe successfully removed");
+    }
+    @PostMapping("{userId}/recipe/save/{recipeId}")
+    public ResponseEntity<String> saveRecipeFromUser(@PathVariable UUID userId, @PathVariable long recipeId) {
+        recipeService.saveRecipeFromUsers(userId, recipeId);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Recipe successfully saved");
     }
 
     @DeleteMapping("{userId}/recipe/{recipeId}")
     public ResponseEntity<String> deleteRecipe(@PathVariable UUID userId, @PathVariable Long recipeId) {
         recipeService.deleteRecipe(userId,recipeId);
-        return ResponseEntity.status(HttpStatus.OK).body("Recipe successfully deleted");
+        return ResponseEntity.status(HttpStatus.OK).body("Recipe successfully removed");
+    }
+    @PutMapping("{userId}/recipe/{recipeId}")
+    public ResponseEntity<RecipeResponse> updateRecipe(@PathVariable UUID userId, @PathVariable Long recipeId, @RequestBody RecipeRequest recipeRequest) {
+        return ResponseEntity.status(HttpStatus.OK).body(recipeService.updateRecipeInfo(userId,recipeId, recipeRequest));
     }
 }
--- backend/receitas.ai/src/main/java/com/leah/receitas/ai/service/RecipeService.java
+++ backend/receitas.ai/src/main/java/com/leah/receitas/ai/service/RecipeService.java
@@ -10,19 +10,15 @@ import com.leah.receitas.ai.repository.RecipeRepository;
 import com.leah.receitas.ai.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Service
 public class RecipeService {
     //TODO
-    // Implementar função de criação de receitas publicas ou privadas
-    // Implementar métodos crud
-    // Alimentar API com receitas externas
-    // Permitir usuário buscar receitas pelo nome
-    // Listar apenas receitas não privadas
     // Permitir o usuário Salvar receitas de terceiros
     // Implementar IA
-    // Verificar dados com DTO. mapear saídas e entradas
     UserService userService;
     UserRepository userRepository;
     RecipeRepository recipeRepository;
@@ -32,9 +28,80 @@ public class RecipeService {
         this.userRepository = userRepository;
         this.recipeRepository = recipeRepository;
     }
+    public void removeRecipeSaved(UUID userId, long recipeId){
+        User user = userService.verifyUser(userId);
+        user.getSavedRecipeList().removeIf(id -> id == recipeId);
+        userRepository.save(user);
+    }
+    public List<RecipeResponse> listSavedRecipes(UUID userId){
+        List<RecipeResponse> tempSavedRecipes = new ArrayList<>();
+        User user = userService.verifyUser(userId);
+        for (long id : user.getSavedRecipeList()){
+            Recipe recipe = recipeRepository.findById(id).orElse(null);
+            RecipeResponse response = new RecipeResponse(recipe.getRecipeName(), recipe.getIngredients(), recipe.getInstructions(), recipe.getIsPrivate(), recipe.getUsernameRecipe());
+            tempSavedRecipes.add(response);
+        }
+        return tempSavedRecipes;
+    }
+    public RecipeResponse saveRecipeFromUsers(UUID userId, long recipeId){
+        User user = userService.verifyUser(userId);
+        Recipe recipe = recipeRepository.findById(recipeId).orElse(null);
+        if (recipe == null && !recipe.getIsPrivate())
+            throw new RecipeNotFoundException("Recipe not found");
+        user.getSavedRecipeList().add(recipe.getId());
+        userRepository.save(user);
+        return new RecipeResponse(recipe.getRecipeName(), recipe.getIngredients(), recipe.getInstructions(), recipe.getIsPrivate(), recipe.getUsernameRecipe());
+    }
+    public List<RecipeResponse> getRecipesByNamePublic(String recipeName) {
+        List<Recipe> recipeList = recipeRepository.findByRecipeName(recipeName);
+        List<RecipeResponse> recipeResponseList = new ArrayList<>();
+        recipeList.stream().filter(recipe -> !recipe.getIsPrivate()).toList().forEach(recipe -> {
+            RecipeResponse recipeResponse = new RecipeResponse(recipe.getRecipeName(), recipe.getInstructions(), recipe.getIngredients(), recipe.getIsPrivate(), recipe.getUsernameRecipe());
+            recipeResponseList.add(recipeResponse);
+        });
+
+        if (recipeResponseList.isEmpty())
+            throw new RecipeNotFoundException("None recipes matches with name: " + recipeName);
+        return recipeResponseList;
+    }
+    public List<RecipeResponse> getUserRecipesByName(UUID userId, String recipeName){
+        User user = userService.verifyUser(userId);
+        List<RecipeResponse> recipeResponseList = new ArrayList<>();
+        user.getRecipeList().stream().filter(recipe -> recipe.getRecipeName().equalsIgnoreCase(recipeName)).toList().forEach(recipe -> {
+            RecipeResponse recipeResponse = new RecipeResponse(recipe.getRecipeName(), recipe.getInstructions(), recipe.getIngredients(), recipe.getIsPrivate(), recipe.getUsernameRecipe());
+            recipeResponseList.add(recipeResponse);
+        });
+        if (recipeResponseList.isEmpty())
+            throw new RecipeNotFoundException("None recipes matches with name: " + recipeName);
+        return recipeResponseList;
+    }
+    public RecipeResponse updateRecipeInfo(UUID userId, long recipeId, RecipeRequest recipeRequest) {
+        User user = userService.verifyUser(userId);
+        Recipe newRecipe = getRecipe(recipeRequest, user);
+        for (Recipe recipe : user.getRecipeList()) {
+            if (recipe.getId() == recipeId) {
+                recipe.setIngredients(newRecipe.getIngredients());
+                recipe.setInstructions(newRecipe.getInstructions());
+                recipe.setIsPrivate(newRecipe.getIsPrivate());
+                recipe.setRecipeName(newRecipe.getRecipeName());
+                recipeRepository.save(recipe);
+                userRepository.save(user);
+                break;
+            }
+        }
+        return new RecipeResponse(recipeRequest.recipeName(), recipeRequest.ingredients(), recipeRequest.instructions(), recipeRequest.isPrivate(), user.getUsername());
+    }
 
     public RecipeResponse createRecipe(RecipeRequest recipeRequest, UUID userId) {
         User user = userService.verifyUser(userId);
+        Recipe recipe = getRecipe(recipeRequest, user);
+        recipeRepository.save(recipe);
+        user.getRecipeList().add(recipe);
+        userRepository.save(user);
+        return new RecipeResponse(recipeRequest.recipeName(), recipeRequest.ingredients(), recipeRequest.instructions(), recipeRequest.isPrivate(), user.getUsername());
+    }
+
+    private Recipe getRecipe(RecipeRequest recipeRequest, User user) {
         Recipe recipe = new Recipe();
         recipe.setRecipeName(recipeRequest.recipeName());
         recipe.setUsernameRecipe(user.getUsername());
@@ -43,10 +110,7 @@ public class RecipeService {
         recipe.setInstructions(recipeRequest.instructions());
         if (recipe.getRecipeName() == null || recipe.getInstructions() == null || recipe.getIngredients() == null)
             throw new MissingFieldsException("Some of the field are missing");
-        userRepository.save(user);
-        user.getRecipeList().add(recipe);
-        recipeRepository.save(recipe);
-        return new RecipeResponse(recipeRequest.recipeName(),recipeRequest.ingredients(),recipeRequest.instructions(),recipeRequest.isPrivate(), user.getUsername());
+        return recipe;
     }
 
     public void deleteRecipe(UUID userId, long recipeId) {
@@ -55,7 +119,7 @@ public class RecipeService {
         if (recipe == null && recipe.getUsernameRecipe().equalsIgnoreCase(user.getUsername()))
             throw new RecipeNotFoundException("Recipe not found");
         user.getRecipeList().remove(recipe);
+        recipeRepository.deleteById(recipeId);
         userRepository.save(user);
-        recipeRepository.deleteById(recipe.getId());
     }
- }
+}
