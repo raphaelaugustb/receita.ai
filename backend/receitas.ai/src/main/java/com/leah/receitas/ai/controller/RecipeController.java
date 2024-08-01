932f57c2eb359c16b0ce29b72b34e83b5218617f
raphaelaugustb <travassosraphael12@gmail.com>
2024-08-01 01:15:50 -0300

    Created routes for save new recipe from another user, delete recipe and list recipes

--- backend/receitas.ai/src/main/java/com/leah/receitas/ai/controller/RecipeController.java
+++ backend/receitas.ai/src/main/java/com/leah/receitas/ai/controller/RecipeController.java
@@ -14,42 +14,54 @@ import java.util.UUID;
 public class RecipeController {
 
     private final RecipeService recipeService;
+
     //TODO
     // Melhorar qualidades da rotas
     // Separar serviços para listar salvos e lista do usuário
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
+
     @GetMapping({"/recipe"})
     public ResponseEntity<List<RecipeResponse>> getRecipePublic(@RequestParam(name = "name") String recipeName) {
         return ResponseEntity.status(HttpStatus.FOUND).body(recipeService.getRecipesByNamePublic(recipeName));
     }
+
     @GetMapping("{userId}/recipe")
-    public ResponseEntity<List<RecipeResponse>> getUserRecipe(@PathVariable UUID userId, @RequestParam(name = "name") String recipeName) {
+    public ResponseEntity<List<RecipeResponse>> getUserRecipeOnList(@PathVariable UUID userId, @RequestParam(name = "name") String recipeName) {
         return ResponseEntity.status(HttpStatus.FOUND).body(recipeService.getUserRecipesByName(userId, recipeName));
     }
+
     @PostMapping("{userId}/recipe")
     public ResponseEntity<RecipeResponse> createRecipe(@PathVariable UUID userId, @RequestBody RecipeRequest recipeRequest) {
         return ResponseEntity.status(HttpStatus.CREATED).body(recipeService.createRecipe(recipeRequest, userId));
     }
+
     @DeleteMapping("{userId}/recipe/save/{recipeId}")
     public ResponseEntity<String> removeSavedRecipe(@PathVariable UUID userId, @PathVariable long recipeId) {
-        recipeService.removeRecipeSaved(userId,recipeId);
+        recipeService.removeRecipeSaved(userId, recipeId);
         return ResponseEntity.status(HttpStatus.OK).body("Recipe successfully removed");
     }
+
     @PostMapping("{userId}/recipe/save/{recipeId}")
-    public ResponseEntity<String> saveRecipeFromUser(@PathVariable UUID userId, @PathVariable long recipeId) {
+    public ResponseEntity<String> saveRecipeFromAnotherUser(@PathVariable UUID userId, @PathVariable long recipeId) {
         recipeService.saveRecipeFromUsers(userId, recipeId);
         return ResponseEntity.status(HttpStatus.CREATED).body("Recipe successfully saved");
     }
 
+    @GetMapping("{userId}/recipe/save")
+    public ResponseEntity<List<RecipeResponse>> getUserSavedRecipes(@PathVariable UUID userId) {
+        return ResponseEntity.status(HttpStatus.FOUND).body(recipeService.listSavedRecipes(userId));
+    }
+
     @DeleteMapping("{userId}/recipe/{recipeId}")
-    public ResponseEntity<String> deleteRecipe(@PathVariable UUID userId, @PathVariable Long recipeId) {
-        recipeService.deleteRecipe(userId,recipeId);
+    public ResponseEntity<String> deleteUserRecipe(@PathVariable UUID userId, @PathVariable Long recipeId) {
+        recipeService.deleteRecipe(userId, recipeId);
         return ResponseEntity.status(HttpStatus.OK).body("Recipe successfully removed");
     }
+
     @PutMapping("{userId}/recipe/{recipeId}")
     public ResponseEntity<RecipeResponse> updateRecipe(@PathVariable UUID userId, @PathVariable Long recipeId, @RequestBody RecipeRequest recipeRequest) {
-        return ResponseEntity.status(HttpStatus.OK).body(recipeService.updateRecipeInfo(userId,recipeId, recipeRequest));
+        return ResponseEntity.status(HttpStatus.OK).body(recipeService.updateRecipeInfo(userId, recipeId, recipeRequest));
     }
 }
