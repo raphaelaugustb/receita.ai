69a81b88029ef442edc6a66ed4f661cefd46b5cc
raphaelaugustb <travassosraphael12@gmail.com>
2024-08-01 01:00:37 -0300

    Update query for listing recipes

--- backend/receitas.ai/src/main/java/com/leah/receitas/ai/repository/RecipeRepository.java
+++ backend/receitas.ai/src/main/java/com/leah/receitas/ai/repository/RecipeRepository.java
@@ -3,7 +3,9 @@ package com.leah.receitas.ai.repository;
 import com.leah.receitas.ai.entity.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-    Recipe findByRecipeName(String recipeName);
+    List<Recipe> findByRecipeName(String recipeName);
 }
