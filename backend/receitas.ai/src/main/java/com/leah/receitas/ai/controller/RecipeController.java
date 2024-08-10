6c248211b7cb249f7a7ca3ed3b10c417c2001ac1
raphaelaugustb <travassosraphael12@gmail.com>
2024-08-10 00:18:42 -0300

    Refactor: Refactoring code for implement Recipe Handler

--- backend/receitas.ai/src/main/java/com/leah/receitas/ai/controller/RecipeController.java
+++ backend/receitas.ai/src/main/java/com/leah/receitas/ai/controller/RecipeController.java
@@ -2,7 +2,7 @@ package com.leah.receitas.ai.controller;
 
 import com.leah.receitas.ai.dto.recipe.RecipeRequest;
 import com.leah.receitas.ai.dto.recipe.RecipeResponse;
-import com.leah.receitas.ai.service.RecipeService;
+import com.leah.receitas.ai.handler.RecipeHandler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,59 +13,57 @@ import java.util.UUID;
 @RestController
 public class RecipeController {
 
-    private final RecipeService recipeService;
-    public RecipeController(RecipeService recipeService) {
-        this.recipeService = recipeService;
+    RecipeHandler recipeHandler;
+
+    public RecipeController(RecipeHandler recipeHandler) {
+        this.recipeHandler = recipeHandler;
     }
 
     //Buscar uma recipe no repositório público
     @GetMapping({"/recipe"})
-    public ResponseEntity<List<RecipeResponse>> getRecipePublic(@RequestParam(name = "name") String recipeName) {
-        return ResponseEntity.status(HttpStatus.FOUND).body(recipeService.getRecipesByNamePublic(recipeName));
+    public ResponseEntity<List<RecipeResponse>> getPublicRecipesByName(@RequestParam(name = "query") String recipeName) {
+        return recipeHandler.queryRecipesByNameHandler(recipeName);
     }
 
-    //Buscar uma recipe no do usuário
-    @GetMapping("{userId}/recipe")
-    public ResponseEntity<List<RecipeResponse>> getUserRecipeOnList(@PathVariable UUID userId, @RequestParam(name = "name") String recipeName) {
-        return ResponseEntity.status(HttpStatus.FOUND).body(recipeService.getUserRecipesByName(userId, recipeName));
+    //Buscar uma recipe na lista do usuário
+    @GetMapping("{username}/recipe")
+    public ResponseEntity<List<RecipeResponse>> getUserRecipeOnListByName(@PathVariable String username, @RequestParam(name = "name") String recipeName) {
+        return recipeHandler.queryRecipeOnUserListHandler(username, recipeName);
     }
 
     //Criar uma nova recipe e endereçar ela um usuário e alimentar a API
     @PostMapping("{userId}/recipe")
     public ResponseEntity<RecipeResponse> createRecipe(@PathVariable UUID userId, @RequestBody RecipeRequest recipeRequest) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(recipeService.createRecipe(recipeRequest, userId));
+        return recipeHandler.createNewRecipeHandler(userId, recipeRequest);
     }
 
     //Remover uma recipe da lista de salvos
     @DeleteMapping("{userId}/recipe/save/{recipeId}")
-    public ResponseEntity<String> removeSavedRecipe(@PathVariable UUID userId, @PathVariable long recipeId) {
-        recipeService.removeRecipeSaved(userId, recipeId);
-        return ResponseEntity.status(HttpStatus.OK).body("Recipe successfully removed");
+    public ResponseEntity<Void> removeSavedRecipe(@PathVariable UUID userId, @PathVariable long recipeId) {
+        return recipeHandler.deleteSavedRecipeHandler(userId, recipeId);
     }
 
     //Salvar um link simbólico pelo Id na lista de recipes salvas
     @PostMapping("{userId}/recipe/save/{recipeId}")
     public ResponseEntity<String> saveRecipeFromAnotherUser(@PathVariable UUID userId, @PathVariable long recipeId) {
-        recipeService.saveRecipeFromUsers(userId, recipeId);
-        return ResponseEntity.status(HttpStatus.CREATED).body("Recipe successfully saved");
+        return recipeHandler.saveRecipeFromUserHandler(userId, recipeId);
     }
 
     //Acessar a lista de links simbólicos e buscar no repositório pelo id as recipes
     @GetMapping("{userId}/recipe/save")
     public ResponseEntity<List<RecipeResponse>> getUserSavedRecipes(@PathVariable UUID userId) {
-        return ResponseEntity.status(HttpStatus.FOUND).body(recipeService.listSavedRecipes(userId));
+        return recipeHandler.getSavedRecipesListHandler(userId);
     }
 
     //Deletar as receitas do usuário
     @DeleteMapping("{userId}/recipe/{recipeId}")
-    public ResponseEntity<String> deleteUserRecipe(@PathVariable UUID userId, @PathVariable Long recipeId) {
-        recipeService.deleteRecipe(userId, recipeId);
-        return ResponseEntity.status(HttpStatus.OK).body("Recipe successfully removed");
+    public ResponseEntity<Void> deleteUserRecipe(@PathVariable UUID userId, @PathVariable Long recipeId) {
+        return recipeHandler.deleteRecipeFromUserList(userId, recipeId);
     }
 
     //Alterar alguma receita do usuário
     @PutMapping("{userId}/recipe/{recipeId}")
     public ResponseEntity<RecipeResponse> updateRecipe(@PathVariable UUID userId, @PathVariable Long recipeId, @RequestBody RecipeRequest recipeRequest) {
-        return ResponseEntity.status(HttpStatus.OK).body(recipeService.updateRecipeInfo(userId, recipeId, recipeRequest));
+        return recipeHandler.updateRecipeInfoHandler(userId,recipeId,recipeRequest);
     }
 }
